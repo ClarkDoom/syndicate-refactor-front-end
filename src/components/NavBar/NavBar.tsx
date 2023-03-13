ccfd380c32b470f9220a055573d78243a4fdf572
branhickman <branhickman@gmail.com>
2023-03-12 18:00:08 -0600

    setup nav bar structure and base css

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,4 +1,7 @@
 // npm modules
+import { useState, useEffect } from 'react';
+
+// npm modules
 import { NavLink } from 'react-router-dom'
 
 // types
@@ -11,22 +14,62 @@ interface NavBarProps {
 
 const NavBar = (props: NavBarProps): JSX.Element => {
   const { user, handleLogout } = props
-  
+
+  const [toggleMenu, setToggleMenu] = useState(false)
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+  const toggleNav = () => {
+    setToggleMenu(!toggleMenu)
+  }
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth)
+    }
+    window.addEventListener('resize', changeWidth)
+    return () => {
+      window.removeEventListener('resize', changeWidth)
+    }
+  }, [])
+
   return (
     <nav>
-      {user ?
-        <ul>
-          <li>Welcome, {user.name}</li>
-          <li><NavLink to="/profiles">Profiles</NavLink></li>
-          <li><NavLink to="/change-password">Change Password</NavLink></li>
-          <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
-        </ul>
-      :
-        <ul>
-          <li><NavLink to="/login">Log In</NavLink></li>
-          <li><NavLink to="/signup">Sign Up</NavLink></li>
-        </ul>
-      }
+      {(toggleMenu || screenWidth > 500) && (
+        <div className="list">
+          {user ?
+            <>
+              <p className="items" id="home-link">
+                <NavLink to="/">
+                  Home
+                </NavLink>
+              </p>
+              <p className="items">
+                <NavLink to="/profiles">
+                  Profiles
+                </NavLink>
+              </p>
+              <p className="items">
+                <NavLink to="/change-password">
+                  Change Password
+                </NavLink>
+              </p>
+              <p className="items">
+                <NavLink to="" onClick={handleLogout}>
+                  LOG OUT
+                </NavLink>
+              </p>
+            </>
+            :
+            <>
+              <p><NavLink to="/login">Log In</NavLink></p>
+              <p><NavLink to="/signup">Sign Up</NavLink></p>
+            </>
+          }
+        </div>
+      )}
+      <button onClick={toggleNav} className="nav-btn">
+        <img src="/nav.png" alt="" />
+      </button>
     </nav>
   )
 }
