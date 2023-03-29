6001436f247dcf16700b0d8e33684933f5849d89
branhickman <branhickman@gmail.com>
2023-03-29 13:32:51 -0600

    update styling on activy panel on Community page

--- src/components/CommunityActivityCard/CommunityActivityCard.tsx
+++ src/components/CommunityActivityCard/CommunityActivityCard.tsx
@@ -5,13 +5,15 @@ const CommunityCard = (props: CommunityCardProps) => {
 
   const { show } = props
 
+  const formattedShowType = show.showType.toUpperCase()
+  const formattedShowName = show.showName.toUpperCase()
+  const formattedUserName = show.profile.userName.toUpperCase()
+
   return (
     <div className={communityActivityCardStyles.card}>
       <img src={`https://www.themoviedb.org/t/p/w188_and_h282_bestv2${show.imageUrl}`} alt="" />
       <div className={communityActivityCardStyles.cardDetails}>
-        <p>{show.showName}</p>
-        <p>Added By: {show.profile.userName}</p>
-        <p>Added To: {show.showType}</p>
+        <p>@{formattedUserName} added {formattedShowName} to their {formattedShowType} list </p>
       </div>
     </div>
   );
