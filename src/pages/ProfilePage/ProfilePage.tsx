b4a3c79fbe8da5a42673d49b6e34cc32cbb555c9
branhickman <branhickman@gmail.com>
2023-03-31 14:25:37 -0600

    setup 2nd layer profilepage styling

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -18,39 +18,76 @@ const Profile = (props: ProfilePageProps) => {
     return (show.showType.toString() === "seen it")
   })
   const reviews = profile.reviews
+  console.log(reviews)
 
   return (
     <div className={profilePageStyles.page}>
-      <h1>Profile Page</h1>
-      <Link to="/edit-profile">
-        Edit Profile
-      </Link>
-      <Link to="/change-password">
-        Change Password
-      </Link>
-      <img src={profile.photo} alt="" />
-      <h2>{profile.userName}</h2>
-      <p>{profile.aboutMe}</p>
-      <h1>Favorites</h1>
-      {favoriteShows.map((show: Show) =>
-        <p>{show.showName}</p>
-      )}
-      <h1>Currently Watching</h1>
-      {currentlyWatching.map((show: Show) =>
-        <p>{show.showName}</p>
-      )}
-      <h1>Show's Seen</h1>
-      {seenIt.map((show: Show) =>
-        <p>{show.showName}</p>
-      )}
-      <h1>Reviews</h1>
-      {reviews.map((review: Review) =>
-        <div>
-          <h2>{review.reviewTitle}</h2>
-          <p>{review.rating}</p>
-          <p>{review.reviewContent}</p>
+      <h1>{profile.userName}</h1>
+      <div className={profilePageStyles.actionButtons}>
+        <Link
+          className={profilePageStyles.actionButton}
+          to="/edit-profile"
+        >
+          Edit Profile
+        </Link>
+        <Link
+          to="/change-password"
+          className={profilePageStyles.actionButton}
+        >
+          Change Password
+        </Link>
+      </div>
+      <div className={profilePageStyles.profileSection}>
+        <img src={profile.photo} alt="" />
+        <div className={profilePageStyles.profileInfo}>
+          <p>{profile.aboutMe}</p>
+          <div className={profilePageStyles.statsHeaders}>
+            <p>Shows Watched</p>
+            <p>Show's in Watchlist</p>
+            <p>Show's in Currently Watching</p>
+          </div>
+          <div className={profilePageStyles.statsData}>
+            <p>999</p>
+            <p>999</p>
+            <p>999</p>
+          </div>
         </div>
-      )}
+      </div>
+      <div className={profilePageStyles.section}>
+        <h1>Favorites</h1>
+        <div className={profilePageStyles.sectionShows}>
+          {favoriteShows.map((show: Show) =>
+            <img src={`https://www.themoviedb.org/t/p/w188_and_h282_bestv2${show.imageUrl}`} alt="TV Show Poster" />
+          )}
+        </div>
+      </div>
+      <div className={profilePageStyles.section}>
+        <h1>Currently Watching</h1>
+        <div className={profilePageStyles.sectionShows}>
+          {currentlyWatching.map((show: Show) =>
+            <img src={`https://www.themoviedb.org/t/p/w188_and_h282_bestv2${show.imageUrl}`} alt="TV Show Poster" />
+          )}
+        </div>
+      </div>
+      <div className={profilePageStyles.section}>
+        <h1>Show's Seen</h1>
+        <div className={profilePageStyles.sectionShows}>
+          {seenIt.map((show: Show) =>
+            <img src={`https://www.themoviedb.org/t/p/w188_and_h282_bestv2${show.imageUrl}`} alt="TV Show Poster" />
+          )}
+        </div>
+      </div>
+      <div className={profilePageStyles.section}>
+        <h1>Reviews</h1>
+        <div className={profilePageStyles.sectionShows}>
+          {reviews.map((review: Review) =>
+            <div className={profilePageStyles.reviews}>
+              <img src={`https://www.themoviedb.org/t/p/w188_and_h282_bestv2${review.reviewOf.imageUrl}`} alt="TV Show Poster" />
+              <p>{review.reviewTitle}</p>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
