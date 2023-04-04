c4baebfab4b88b6e5707309c0b959c62b8554e32
branhickman <branhickman@gmail.com>
2023-04-04 17:18:43 -0600

    refactor link setup on search results page

--- src/components/ResultCard/ResultCard.tsx
+++ src/components/ResultCard/ResultCard.tsx
@@ -9,15 +9,14 @@ const ResultCard = (props: ResultCardProps) => {
   const { result } = props 
 
   return (
-    <div className={resultCardStyles.card}>
+      <Link to="/tv-show-result" state={{ resultId: result.id }} className={resultCardStyles.card}>
       <img src={`https://www.themoviedb.org/t/p/w188_and_h282_bestv2${result.poster_path}`} alt="" />
+        
       <p>
-        <Link to="/tv-show-result" state={{ resultId: result.id }}>
           {result.name}
-        </Link>
       </p>
       <p>{result.first_air_date}</p>
-    </div>
+      </Link>
   );
 }
 
